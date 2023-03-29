aff6cbaabbbc135ce869b7ecade0953d94a887dd
Andre <andreivanp@gmail.com>
2023-03-29 14:25:53 +0700

    FIX disabled button 2

--- src/pages/Dashboard/ActivityDetail.js
+++ src/pages/Dashboard/ActivityDetail.js
@@ -386,7 +386,7 @@ const ActivityDetail = () => {
 								Simpan
 							</Button>
 						) : (
-							<Button data-cy="modal-add-save-button" onClick={handleAddList} variant="primary" disabled>
+							<Button data-cy="modal-add-save-button" onClick={handleAddList} variant="primary">
 								Simpan
 							</Button>
 						)}
